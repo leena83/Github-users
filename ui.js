b3d717e08f359fd6a150e1235e8c7fa2ac85e3b9
Leena Mkarem <manikalina83@gmail.com>
2019-02-28 17:01:14 +0100

    changed color

--- ui.js
+++ ui.js
@@ -9,16 +9,16 @@ class UI {
     <div class="card card-body mb-3">
     <div class="row">
       <div class="col-md-3">
-        <img src="${user.avatar_url}" class="img-fluid mb-2" />
+        <img src="${user.avatar_url}" class="img-fluid mb-3" />
         <a
           href="${user.html_url}"
           target="_blank"
-          class="btn btn-primary btn-block mb-4"
+          class="btn btn-primary btn-block mb-2"
           >View Profile</a
         >
       </div>
       <div class="col-md-9">
-        <span class="badge badge-primary"
+       <span class="badge badge-primary"
           >Public Repos: ${user.public_repos}</span
         >
         <span class="badge badge-primary"
@@ -27,7 +27,7 @@ class UI {
         <span class="badge badge-primary">Followers: ${user.followers}</span>
         <span class="badge badge-primary">Following: ${user.following}</span>
         <br />
-        <ul class="list-group">
+        <ul class="list-group ">
           <li class="list-group-item">Company:${user.company}</li>
           <li class="list-group-item">Website/Blog:${user.blog}</li>
           <li class="list-group-item">Location:${user.location}</li>
@@ -48,7 +48,9 @@ class UI {
     <div class="card card-body mb-2">
       <div class ="row">
         <div class="col-md-6">
-         <a href="${repo.html_url}" target="_blank" >${repo.name} </a>
+         <a href="${repo.html_url}"class="dark" target="_blank" >${
+        repo.name
+      } </a>
         </div>
         <div class="col-md-6">
         <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
